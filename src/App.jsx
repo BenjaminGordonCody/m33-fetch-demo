575098870b09423effc920cb1cf927f91d9dfdc7
BenjaminGordonCody <b.gordoncody@gmail.com>
2022-01-25 11:24:10 +0000

    api now connects to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [fact, setFact] = useState("")
+  
   const handleFetch = async () => {
     const response = await fetch("https://cat-fact.herokuapp.com/facts/random")
     const data = await response.json()
-    console.log(data)
+    setFact(data.text)
 
   }
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{fact}</h1>
       <button onClick={handleFetch}>Fetch</button> 
     </div>
   );
